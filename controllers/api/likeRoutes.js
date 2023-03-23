f8316257c418219ecab4a6c86d48dc2fb19226f4
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-03-23 06:20:51 -0700

    fixed comment, tag, and post routes

--- controllers/api/likeRoutes.js
+++ controllers/api/likeRoutes.js
@@ -1,2 +1,13 @@
 // const router = require('express').Router();
-// const { Like } = require('../../models');
+// const { Like, Post, User } = require('../../models');
+// const withAuth = require('../../utils/auth');
+
+// router.put('/', withAuth, async (req, res) => {
+//     try {
+//         const likeData = await Like.update
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
+
+// module.exports = router;
