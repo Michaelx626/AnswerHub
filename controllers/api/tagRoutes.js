ea6179567d3907dac26a203ed2026665bb616154
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-03-23 06:20:51 -0700

    fixed comment, tag, and post routes

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -1,15 +1,17 @@
-// const router = require('express').Router();
-// const { Tag, Post, PostTag } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const router = require('express').Router();
+const { Tag, Post, PostTag, User, UserTag } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// router.get('/:name', withAuth, async (req, res) => {
-//     try {
-//         const tagData = await Tag.findAll({where: {name: req.params.name}, 
-//             include: [{ model: Post, through: PostTag }],
-//         });
+router.get('/:name', withAuth, async (req, res) => {
+    try {
+        const tagData = await Tag.findAll({where: {name: req.params.name}, 
+            include: [{ model: Post, through: PostTag }, { model: User, through: UserTag }],
+        });
 
-//         res.status(200).json(tagData);
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
+        const tags = tagData.map((tag) => tag.get({ plain: true }));
+
+        res.render('tag', { tags });
+    } catch (error) {
+        console.error(error);
+    }
+});
