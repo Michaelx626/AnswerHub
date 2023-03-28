716c595077e3358b57d696e7f6b8eb668ac4387e
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-03-28 01:49:05 -0700

    like routes done! ask about location.reload

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
-// const likeRoutes = require('./likeRoutes');
+const likeRoutes = require('./likeRoutes');
 // const tagRoutes = require('./tagRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
-// router.use('/likes', likeRoutes);
+router.use('/likes', likeRoutes);
 // router.use('/tags', tagRoutes);
 
 module.exports = router;
