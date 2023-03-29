6e3b94f3adca8976c2e45d45b8bc397186f5b307
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-03-29 02:46:56 -0700

    deleting extra files

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
-// const botRoutes = require('./botRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/bot', botRoutes);
 
 module.exports = router;
