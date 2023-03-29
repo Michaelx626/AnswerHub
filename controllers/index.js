76fc3705567ed3f9b1ad9765465212da433ebc38
Michaelx626 <118804354+Michaelx626@users.noreply.github.com>
2023-03-29 03:04:23 -0700

    added partials to all hbs

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
+const aiRoutes = require('./AIroute');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/airesponse', aiRoutes);
 
 module.exports = router;
