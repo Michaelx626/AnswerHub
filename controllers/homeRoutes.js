b092cc4a67af9e193b885426bde62ba69eb8d5f1
Xiaojia <annacl1822@gmail.com>
2023-03-29 14:15:00 -0700

    update homepage responsiveness

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -91,7 +91,7 @@ router.get('/profile', async (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/homepage');
     return;
   }
 
